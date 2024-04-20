3ca61ff06d2fbe1cc950cf855f6f67d092a4ccec
Shubham Sharma <shubhamku044@gmail.com>
2024-04-20 21:20:52 +0530

    food router create

--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shubhamku044/restaurant-management-system/controllers"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods", controllers.GetFoods())
+	incomingRoutes.GET("/foods/:id", controllers.GetFood())
+	incomingRoutes.POST("/foods", controllers.CreateFood())
+	incomingRoutes.PATCH("/foods/:id", controllers.UpdateFood())
+}
