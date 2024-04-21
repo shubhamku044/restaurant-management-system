902e49aa3b0669d6db19120e64710e4335706a78
Shubham Sharma <shubhamku044@gmail.com>
2024-04-21 12:01:49 +0530

    add database

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,41 @@
 package database
+
+import (
+	"context"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+)
+
+func DBInstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	MongoDB := os.Getenv("MONGO_URI")
+	serverApi := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(MongoDB).SetServerAPIOptions(serverApi)
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Fatalf("Error creating mongo client: %v", err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			log.Fatalf("Error disconnecting from mongo client: %v", err)
+		}
+	}()
+	if err := client.Database("restaurant").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		log.Fatalf("Error pinging mongo client: %v", err)
+	}
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+	return collection
+}
