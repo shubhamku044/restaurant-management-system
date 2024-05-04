347e542cf6f2d942bf902c96b87d6cf7f7c2bb9a
Shubham Sharma <shubhamku044@gmail.com>
2024-05-04 13:57:47 +0530

    middleware code update

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,9 +1,34 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shubhamku044/restaurant-management-system/helpers"
+)
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Do some auth stuff
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helpers.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
 	}
 }
